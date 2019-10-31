4dbe6cda71322d867e589871992904362c6df6a5
Glitch (xula-polistock) <none>
2019-10-31 17:39:31 +0000

    Atualizando diretorios das páginas

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ import bodyParser from 'body-parser';
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'public'));
-app.use('/', Express.static('/app/src/public/'));
+app.use('/', Express.static(__dirname + '/public/'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
